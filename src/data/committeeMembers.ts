682b1a14007782dd4fd7ce90b07d88c3e5089233
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-12-02 21:42:00 +0900

    doc(about): Asia Committee 임시 삭제

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -1,40 +1,10 @@
 
-import uca24Logo from "@assets/logo.svg";
+import uck24Logo from "@assets/logo.svg";
 export const committeeMembers = [
     {
         name: "Youngbin Han",
-        team: "Global team & Ubuntu Korea Community, Ubuntu LoCo Council",
+        team: "Ubuntu Korea Community, Ubuntu LoCo Council",
         role: "Allrounder - Usually sponsorship, web and content review",
         profileImageUrl: "https://avatars.githubusercontent.com/u/1916739?v=4",
-    },
-    {
-        name: "Khairul Aizat Bin Kamarudzzaman",
-        team: "Global team & Ubuntu Malaysia, Ubuntu Membership Board",
-        role: "Sponsorship, Web, Content",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/405473?v=4",
-    },
-    {
-        name: "Muhd Syazwan",
-        team: "Global team & Ubuntu Malaysia",
-        role: "Web, Content",
-        profileImageUrl: uca24Logo.src,
-    },
-    {
-        name: "RJ Hsiao",
-        team: "Global team & Ubuntu Taiwan",
-        role: "Content",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/236116?v=4",
-    },
-    {
-        name: "Aryan Kaushik",
-        team: "Local team & Ubuntu India, GNOME Foundation member, GNOME UG Delhi-NCR",
-        role: "Local team Chief, Sponsorship, Venue, Content",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/34372791?v=4",
-    },
-    {
-        name: "Bhavanishankar Ravindra",
-        team: "Local team & Ubuntu India, Ubuntu LoCo Council",
-        role: "Local team Chief, Sponsorship, Venue, Content",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/4153385?v=4",
     }
 ]
