c8a038ccec18e46e69d15bbdafa19a86e8ba27f3
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-04-29 00:26:25 +0900

    준비위원회 구성원 수록

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -1,10 +1,127 @@
-
+import * as m from "../paraglide/messages.js";
 import uck24Logo from "@assets/logo.svg";
 export const committeeMembers = [
     {
+        name: "Junsang Mun",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_venue_safety,
+            m.role_marketing_design
+        ],
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/junmoon/144/12427_2.png"
+    },
+    {
         name: "Youngbin Han",
         team: "Ubuntu Korea Community, Ubuntu LoCo Council",
-        role: "Allrounder - Usually sponsorship, web and content review",
+        role: [
+            m.role_sponsorship_finances,
+            m.role_general,
+            m.role_contents,
+            m.role_web_video,
+            m.role_venue_safety
+        ],
         profileImageUrl: "https://avatars.githubusercontent.com/u/1916739?v=4",
+    },
+    {
+        name: "Minseong Cho",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_general,
+            m.role_venue_safety,
+            m.role_sponsorship_finances
+        ],
+        profileImageUrl: "https://avatars.githubusercontent.com/u/52643858?v=4"
+    },
+    {
+        name: "Sangbin Lim",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_marketing_design,
+            m.role_web_video,
+            m.role_venue_safety
+        ],
+        profileImageUrl: "https://avatars.githubusercontent.com/u/125565943?v=4"
+    },
+    {
+        name: "Jungmin Yoon",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_web_video,
+            m.role_marketing_design
+        ],
+        profileImageUrl: "https://avatars.githubusercontent.com/u/7126454?v=4"
+    },
+    {
+        name: "Giyeon Bang",
+        team: "C++ Korea",
+        role: [
+            m.role_web_video,
+            m.role_venue_safety
+        ],
+        profileImageUrl: "https://2022.ubucon.asia/about/giyeon_bang.jpg"
+    },
+    {
+        name: "Gyuseok Jung",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_contents,
+            m.role_web_video
+        ],
+        profileImageUrl: "https://avatars.githubusercontent.com/u/9061758?v=4"
+    },
+    {
+        name: "Sangkon Han",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_contents,
+            m.role_web_video
+        ],
+        profileImageUrl: "https://2022.ubucon.asia/about/sangkon_han.jpg"
+    },
+    {
+        name: "Yeonguk Choo",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_venue_safety,
+            m.role_sponsorship_finances
+        ],
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/choo121600/144/12050_2.png"
+    },
+    {
+        name: "Hoehyeong Jung",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_web_video,
+            m.role_contents
+        ],
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/hothead-jung/144/12047_2.png"
+    },
+    {
+        name: "Tae Yong Lee",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_venue_safety,
+            m.role_contents,
+            m.role_sponsorship_finances
+        ],
+        profileImageUrl: "https://2024.ubuntu-kr.org/avatars/taeyong.jpg"
+    },
+    {
+        name: "Soyeon Kwon",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_marketing_design,
+            m.role_web_video
+        ],
+        profileImageUrl: "https://2024.ubuntu-kr.org/avatars/soyeon.png"
+    },
+    {
+        name: "Euiseo Cha",
+        team: "Ubuntu Korea Community",
+        role: [
+            m.role_sponsorship_finances,
+            m.role_contents
+        ],
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/zeroday0619/144/12832_2.png"
     }
 ]
