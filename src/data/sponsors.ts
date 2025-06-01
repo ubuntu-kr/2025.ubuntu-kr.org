6441da6eea2bcba64286656720937e696fee28e7
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-06-01 21:01:30 +0900

    Microsoft Korea 후원사 추가

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,6 +1,7 @@
 
 import logoPlaceHolder from "@assets/logo_placeholder.svg";
 import canonicalLogo from "@assets/sponsors/ubuntu.svg";
+import microsoftLogo from "@assets/sponsors/microsoft.png";
 
 
 export const sponsors = [
@@ -16,6 +17,22 @@ export const sponsors = [
                 logoUrl: canonicalLogo.src,
                 url: "https://ubuntu.com/",
                 description: "Canonical은 대부분의 퍼블릭 클라우드 워크로드를 위한 OS이자 스마트 게이트웨이, 자율 주행 자동차 및 Advanced robot 등 새롭게 떠오르는 분야를 위한 OS인 Ubuntu를 개발 및 배포하는 회사입니다. Canonical은 상용 Ubuntu 사용자에게 엔터프라이즈 보안, 지원 및 서비스를 제공합니다."
+            },
+            
+        ]
+    },
+    {
+        sponsorLevelName: "Venue",
+        sizeOnLargeScreen: 5,
+        sizeOnMediumScreen: 5,
+        sizeOnSmallScreen: 4,
+        showPopup: true,
+        list: [
+            {
+                name: "Microsoft Korea",
+                logoUrl: microsoftLogo.src,
+                url: "https://azure.microsoft.com/en-us/ubuntu/?ocid=AID3035196",
+                description: "Microsoft는 지구상의 모든 사람과 조직이 더 많은 것을 성취할 수 있도록 역량을 지원하는 것을 사명으로 삼고 있습니다. Ubuntu 기반 워크로드는 Azure에서 가장 빠르게 성장하고 있는 워크로드 중 하나입니다. Microsoft는 Canonical과 제휴하여 Azure에서 동작하는 Ubuntu와 오픈 소스 커뮤니티 참여를 통해 Azure에 최적화된 커널, 개선된 성능, 고급 보안, 자동 패치 및 업데이트, 유연한 가격 책정 옵션을 제공합니다."
             },
             
         ]
